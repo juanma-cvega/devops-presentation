781819582ff68a4e80714263a4d92609b36cb3ea
Juan Manuel Carnicero Vega <juanma.cvega@gmail.com>
2022-04-11 18:01:09 +0200

    Add demo controller

---
+++ src/main/java/com/example/demo/api/DemoController.java
@@ -0,0 +1,14 @@
+package com.example.demo.api;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class DemoController {
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public String helloWorld() {
+        return "Hello world";
+    }
+}
